7b9a167467474bded9d51b9a3a7572e4b972d41b
GatoMiau03 <d.benaldo03@ufromail.cl>
2024-04-19 00:28:00 -0400

    Integracion del codigo de Luis y Diego, se realizo un menu para ambos para verificar que funcionaran.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -85,6 +85,152 @@ public class Main {
         return numero;
     }
 
+    public static int areaCuadrado(int lado) {
+        try {
+            if (lado <= 0) {
+                throw new IllegalArgumentException("El lado no puede ser 0, negativo ni una letra.");
+            }
+            return lado * lado;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static int perimetroCuadrado(int lado) {
+        try {
+            if (lado <= 0) {
+                throw new IllegalArgumentException("El lado no puede ser 0, negativo ni una letra.");
+            }
+            return lado * 4;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static int areaRectangulo(int base, int altura) {
+        try {
+            if (base <= 0 || altura <= 0) {
+                throw new IllegalArgumentException("La base y la altura no pueden ser 0, negativas ni letras.");
+            }
+            return base * altura;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static int perimetroRectangulo(int base, int altura) {
+        try {
+            if (base <= 0 || altura <= 0) {
+                throw new IllegalArgumentException("La base y la altura no pueden ser 0, negativas ni letras.");
+            }
+            return (base * 2) + (altura * 2);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static double areaCirculo(int radio) {
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser 0, negativo ni una letra.");
+            }
+            return Math.PI * (radio * radio);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static double perimetroCirculo(int radio) {
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser 0, negativo ni una letra.");
+            }
+            return 2 * Math.PI * radio;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static double areaEsfera(int radio) {
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser 0, negativo ni una letra.");
+            }
+            return 4 * Math.PI * (radio * radio);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static double volumenEsfera(int radio) {
+        try {
+            if (radio <= 0) {
+                throw new IllegalArgumentException("El radio no puede ser 0, negativo ni una letra.");
+            }
+            double aa = (double) 4 / 3;
+            return (aa) * Math.PI * (radio * radio * radio);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static int areaCubo(int lado) {
+        try {
+            if (lado <= 0) {
+                throw new IllegalArgumentException("El lado no puede ser 0, negativo ni una letra.");
+            }
+            return 6 * (lado * lado);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static int volumenCubo(int lado) {
+        try {
+            if (lado <= 0) {
+                throw new IllegalArgumentException("El lado no puede ser 0, negativo ni una letra.");
+            }
+            return lado * lado * lado;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static double areaCono(int radio, int generatriz) {
+        try {
+            if (radio <= 0 || generatriz <= 0) {
+                throw new IllegalArgumentException("El radio y la generatriz no pueden ser 0, negativos ni letras.");
+            }
+            return (radio + generatriz)* Math.PI * radio;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
+    public static double volumenCono(int radio, int altura) {
+        try {
+            if (radio <= 0 || altura <= 0) {
+                throw new IllegalArgumentException("El radio y la altura no pueden ser 0, negativos ni letras.");
+            }
+            double aaa = (double) 1 / 3;
+            return aaa * Math.PI * (radio * radio) * altura;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+            return -1;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcion = 0;
@@ -101,6 +247,18 @@ public class Main {
             System.out.println("7. Potencia");
             System.out.println("8. Porcentaje");
             System.out.println("9. Soluciones cuadráticas");
+            System.out.println("10. Área de un cuadrado");
+            System.out.println("11. Perímetro de un cuadrado");
+            System.out.println("12. Área de un rectángulo");
+            System.out.println("13. Perímetro de un rectángulo");
+            System.out.println("14. Área de un círculo");
+            System.out.println("15. Perímetro de un círculo");
+            System.out.println("16. Área de una esfera");
+            System.out.println("17. Volumen de una esfera");
+            System.out.println("18. Área de un cubo");
+            System.out.println("19. Volumen de un cubo");
+            System.out.println("20. Área de un cono");
+            System.out.println("21. Volumen de un cono");
             System.out.println("0. Salir");
 
             try {
@@ -177,6 +335,74 @@ public class Main {
                         if (!Double.isNaN(soluciones[0]) && !Double.isNaN(soluciones[1])) {
                             System.out.println("Soluciones: x1 = " + soluciones[0] + ", x2 = " + soluciones[1]);
                         }
+                        break;
+                    case 10:
+                        System.out.print("Ingresa el lado del cuadrado: ");
+                        int ladoCuadrado = scanner.nextInt();
+                        System.out.println("Área: " + areaCuadrado(ladoCuadrado));
+                        break;
+                    case 11:
+                        System.out.print("Ingresa el lado del cuadrado: ");
+                        int ladoCuadradoPerimetro = scanner.nextInt();
+                        System.out.println("Perímetro: " + perimetroCuadrado(ladoCuadradoPerimetro));
+                        break;
+                    case 12:
+                        System.out.print("Ingresa la base del rectángulo: ");
+                        int baseRectangulo = scanner.nextInt();
+                        System.out.print("Ingresa la altura del rectángulo: ");
+                        int alturaRectangulo = scanner.nextInt();
+                        System.out.println("Área: " + areaRectangulo(baseRectangulo, alturaRectangulo));
+                        break;
+                    case 13:
+                        System.out.print("Ingresa la base del rectángulo: ");
+                        int baseRectanguloPerimetro = scanner.nextInt();
+                        System.out.print("Ingresa la altura del rectángulo: ");
+                        int alturaRectanguloPerimetro = scanner.nextInt();
+                        System.out.println("Perímetro: " + perimetroRectangulo(baseRectanguloPerimetro, alturaRectanguloPerimetro));
+                        break;
+                    case 14:
+                        System.out.print("Ingresa el radio del círculo: ");
+                        int radioCirculo = scanner.nextInt();
+                        System.out.println("Área: " + areaCirculo(radioCirculo));
+                        break;
+                    case 15:
+                        System.out.print("Ingresa el radio del círculo: ");
+                        int radioCirculoPerimetro = scanner.nextInt();
+                        System.out.println("Perímetro: " + perimetroCirculo(radioCirculoPerimetro));
+                        break;
+                    case 16:
+                        System.out.print("Ingresa el radio de la esfera: ");
+                        int radioEsfera = scanner.nextInt();
+                        System.out.println("Área: " + areaEsfera(radioEsfera));
+                        break;
+                    case 17:
+                        System.out.print("Ingresa el radio de la esfera: ");
+                        int radioEsferaVolumen = scanner.nextInt();
+                        System.out.println("Volumen: " + volumenEsfera(radioEsferaVolumen));
+                        break;
+                    case 18:
+                        System.out.print("Ingresa el lado del cubo: ");
+                        int ladoCubo = scanner.nextInt();
+                        System.out.println("Área: " + areaCubo(ladoCubo));
+                        break;
+                    case 19:
+                        System.out.print("Ingresa el lado del cubo: ");
+                        int ladoCuboVolumen = scanner.nextInt();
+                        System.out.println("Volumen: " + volumenCubo(ladoCuboVolumen));
+                        break;
+                    case 20:
+                        System.out.print("Ingresa el radio del cono: ");
+                        int radioCono = scanner.nextInt();
+                        System.out.print("Ingresa la generatriz del cono: ");
+                        int generatrizCono = scanner.nextInt();
+                        System.out.println("Área: " + areaCono(radioCono, generatrizCono));
+                        break;
+                    case 21:
+                        System.out.print("Ingresa el radio del cono: ");
+                        int radioConoVolumen = scanner.nextInt();
+                        System.out.print("Ingresa la altura del cono: ");
+                        int alturaConoVolumen = scanner.nextInt();
+                        System.out.println("Volumen: " + volumenCono(radioConoVolumen, alturaConoVolumen));
                         break;
                     case 0:
                         System.out.println("Saliendo...");
