867a4ab19e730ce4cf441bb54817730498e6607d
Toritou <l.toro05@ufromail.cl>
2024-04-18 23:55:14 -0400

    errores en las pruebas unitarias (solucionado)

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,76 +6,90 @@ class MainTest {
 
     @Test
     void areaCuadrado() {
-        assertEquals(25, Main.areaCuadrado(5));
-        assertEquals(0, Main.areaCuadrado(0));
+        int lado = 5;
+        int expectedArea = 25;
+        assertEquals(expectedArea, Main.areaCuadrado(lado));
     }
 
     @Test
     void perimetroCuadrado() {
-        assertEquals(20, Main.perimetroCuadrado(5));
-        assertEquals(0, Main.perimetroCuadrado(0));
+        int lado = 5;
+        int expectedPerimeter = 20;
+        assertEquals(expectedPerimeter, Main.perimetroCuadrado(lado));
     }
 
     @Test
     void areaRectangulo() {
-        assertEquals(20, Main.areaRectangulo(4, 5));
-        assertEquals(0, Main.areaRectangulo(0, 5));
+        int base = 4;
+        int altura = 5;
+        int expectedArea = 20;
+        assertEquals(expectedArea, Main.areaRectangulo(base, altura));
     }
 
     @Test
     void perimetroRectangulo() {
-        assertEquals(18, Main.perimetroRectangulo(4, 5));
-        assertEquals(10, Main.perimetroRectangulo(0, 5));
+        int base = 4;
+        int altura = 5;
+        int expectedPerimeter = 18;
+        assertEquals(expectedPerimeter, Main.perimetroRectangulo(base, altura));
     }
 
     @Test
     void areaCirculo() {
-        assertEquals(78.5398, Main.areaCirculo(5), 0.0001);
-        assertEquals(0, Main.areaCirculo(0), 0.0001);
+        int radio = 5;
+        double expectedArea = 78.5398;
+        assertEquals(expectedArea, Main.areaCirculo(radio), 0.0001);
     }
 
     @Test
     void perimetroCirculo() {
-        assertEquals(31.4159, Main.perimetroCirculo(5), 0.0001);
-        assertEquals(0, Main.perimetroCirculo(0), 0.0001);
+        int radio = 5;
+        double expectedPerimeter = 31.4159;
+        assertEquals(expectedPerimeter, Main.perimetroCirculo(radio), 0.0001);
     }
 
     @Test
     void areaEsfera() {
-        assertEquals(314.1593, Main.areaEsfera(5), 0.0001);
-        assertEquals(0, Main.areaEsfera(0), 0.0001);
+        int radio = 5;
+        double expectedArea = 314.1593;
+        assertEquals(expectedArea, Main.areaEsfera(radio), 0.0001);
     }
 
     @Test
     void volumenEsfera() {
-        assertEquals(523.5988, Main.volumenEsfera(5), 0.0001);
-        assertEquals(0, Main.volumenEsfera(0), 0.0001);
+        int radio = 5;
+        double expectedVolume = 523.5988;
+        assertEquals(expectedVolume, Main.volumenEsfera(radio), 0.0001);
     }
 
     @Test
     void areaCubo() {
-        assertEquals(150, Main.areaCubo(5));
-        assertEquals(0, Main.areaCubo(0));
+        int lado = 5;
+        int expectedArea = 150;
+        assertEquals(expectedArea, Main.areaCubo(lado));
     }
 
     @Test
     void volumenCubo() {
-        assertEquals(125, Main.volumenCubo(5));
-        assertEquals(0, Main.volumenCubo(0));
+        int lado = 5;
+        int expectedVolume = 125;
+        assertEquals(expectedVolume, Main.volumenCubo(lado));
     }
 
     @Test
     void areaCono() {
-        double radio = 5;
-        double generatriz = 10;
+        int radio = 5;
+        int generatriz = 10;
         double expectedArea = Math.PI * 75;
-        assertEquals(expectedArea, Main.areaCono((int) radio, (int) generatriz), 0.0001);
+        assertEquals(expectedArea, Main.areaCono(radio, generatriz), 0.0001);
     }
 
     @Test
     void volumenCono() {
-        assertEquals(261.7994, Main.volumenCono(5, 10), 0.0001);
-        assertEquals(0, Main.volumenCono(0, 0), 0.0001);
+        int radio = 5;
+        int altura = 10;
+        double expectedVolume = 261.7994;
+        assertEquals(expectedVolume, Main.volumenCono(radio, altura), 0.0001);
     }
 
 }
